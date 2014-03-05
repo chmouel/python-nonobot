c294102a4cd42ace8145fe6eac61d5554980b72a
Chmouel Boudjnah <chmouel@chmouel.com>
2014-03-05 10:00:23 +0100

    Use pbr/tox
    Make things easier for tests

--- setup.py
+++ setup.py
@@ -1,43 +1,21 @@
-# stevedore/example/setup.py
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# Copyright (c) 2010-2011 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-setup(
-    name='nonobot',
-    version='0.1',
+import setuptools
 
-    description='The nono bot',
 
-    author='Chmouel Boudjnah',
-    author_email='chmouel@chmouel.com',
-
-    url='https://github.com/chmouel/nonobot',
-    download_url='https://github.com/chmouel/nonobot/tarball/master',
-
-    classifiers=['Development Status :: 3 - Alpha',
-                 'License :: OSI Approved :: Apache Software License',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.2',
-                 'Programming Language :: Python :: 3.3',
-    ],
-
-    platforms=['Any'],
-
-    scripts=[],
-
-    provides=['nonobot',
-    ],
-
-    packages=find_packages(),
-    include_package_data=True,
-
-    entry_points={
-        'nonobot.plugins': [
-            'bug = nonobot.plugins.bug:Bug',
-        ],
-    },
-
-    zip_safe=False,
-)
+setuptools.setup(setup_requires=['pbr>=0.5.21,<1.0'],
+                 pbr=True)
