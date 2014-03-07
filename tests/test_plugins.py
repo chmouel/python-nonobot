800ad5813c9ed76362c0aebd7d279c54931baca3
Chmouel Boudjnah <chmouel@chmouel.com>
2014-03-07 12:10:17 +0100

    Use a contextmanager for cleanup tempdirs.

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -13,11 +13,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import optparse
-import shutil
-import tempfile
 import unittest
 
+import fixtures
 import nonobot.plugins
+
 
 SAMPLE_ARGUMENT_PLUGIN = """
 def _arguments(o):
@@ -47,71 +47,42 @@ class Plugin:
 
 class PluginTest(unittest.TestCase):
     def test_get_all_plugin_modules(self):
-        try:
-            path = tempfile.mkdtemp()
+        with fixtures.cleaned_tempdir() as path:
             test_file = path + "/t1.py"
             open(test_file, 'w').write("")
             test_file2 = path + "/t2.py"
             open(test_file2, 'w').write("")
             ret = [x for x in nonobot.plugins.get_all_plugin_modules(path)]
             self.assertEqual(len(ret), 2)
-        finally:
-            try:
-                shutil.rmtree(path)
-            except OSError as exc:
-                if exc.errno != 2:
-                    raise
 
     def test_get_all_plugin_modules_only_py(self):
-        try:
-            path = tempfile.mkdtemp()
+        with fixtures.cleaned_tempdir() as path:
             test_file = path + "/t1.rb"
             open(test_file, 'w').write("")
             test_file2 = path + "/t2.py"
             open(test_file2, 'w').write("")
             ret = [x for x in nonobot.plugins.get_all_plugin_modules(path)]
             self.assertEqual(len(ret), 1)
-        finally:
-            try:
-                shutil.rmtree(path)
-            except OSError as exc:
-                if exc.errno != 2:
-                    raise
 
     def test_get_all_plugin_modules_no__init__(self):
-        try:
-            path = tempfile.mkdtemp()
+        with fixtures.cleaned_tempdir() as path:
             test_file = path + "__init__.py"
             open(test_file, 'w').write("")
             test_file2 = path + "/t2.py"
             open(test_file2, 'w').write("")
             ret = [x for x in nonobot.plugins.get_all_plugin_modules(path)]
             self.assertEqual(len(ret), 1)
-        finally:
-            try:
-                shutil.rmtree(path)
-            except OSError as exc:
-                if exc.errno != 2:
-                    raise
 
     def test_plugin_add_extra_options(self):
-        try:
-            path = tempfile.mkdtemp()
+        with fixtures.cleaned_tempdir() as path:
             test_file = path + "/t1.py"
             open(test_file, 'w').write(SAMPLE_ARGUMENT_PLUGIN)
             optp = optparse.OptionParser()
             nonobot.plugins.plugins_add_extra_options(path, optp)
             self.assertTrue(optp.has_option('--blah'))
-        finally:
-            try:
-                shutil.rmtree(path)
-            except OSError as exc:
-                if exc.errno != 2:
-                    raise
 
     def test_plugin_methods(self):
-        try:
-            path = tempfile.mkdtemp()
+        with fixtures.cleaned_tempdir() as path:
             test_file = path + "/t1.py"
             open(test_file, 'w').write(SAMPLE_PLUGIN_METHOD)
             plugins = nonobot.plugins.get_plugins_methods(path, {'foo: bar'})
@@ -128,16 +99,9 @@ class PluginTest(unittest.TestCase):
             self.assertEqual(len(plugins['help']), 1)
             self.assertEqual(plugins['help'],
                              ['[t1] foo_doc: THIS IS SOME DOC'])
-        finally:
-            try:
-                shutil.rmtree(path)
-            except OSError as exc:
-                if exc.errno != 2:
-                    raise
 
     def test_plugin_stream_method(self):
-        try:
-            path = tempfile.mkdtemp()
+        with fixtures.cleaned_tempdir() as path:
             test_file = path + "/tstream.py"
             open(test_file, 'w').write(SAMPLE_PLUGIN_STREAM)
             plugins = nonobot.plugins.get_plugins_methods(path, {'foo: bar'})
@@ -152,12 +116,6 @@ class PluginTest(unittest.TestCase):
             self.assertIn('help', plugins)
             self.assertEqual(len(plugins['help']), 1)
             self.assertEqual(plugins['help'], ['[tstream] DOC STREAM'])
-        finally:
-            try:
-                shutil.rmtree(path)
-            except OSError as exc:
-                if exc.errno != 2:
-                    raise
 
     def test_plugin_class(self):
         config = {'hello': 'moto'}
