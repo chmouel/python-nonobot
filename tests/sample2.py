559bcb961a8eb842b4bb24ae554b876812c68ae9
Chmouel Boudjnah <chmouel@chmouel.com>
2014-03-15 10:01:46 +0100

    Actually add the sample plugins files

---
+++ tests/sample2.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Author: Chmouel Boudjnah <chmouel@chmouel.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def _arguments(o):
+    o.add_option('--blah')
+
+
+class Plugin(object):
+    def __init__(self, config):
+        pass
+
+    def foo2(self, msg):
+        return "Hello World"
+
+    def foo2_multiple(self, msg):
+        return ["Hello", "World"]
+
+    def foo2_doc(self, msg):
+        "THIS IS SOME DOC"
+        pass
+
+    def stream(self, msg):
+        """DOC STREAM."""
+        if msg['body'] != 'callstream2':
+            return
+        return "Hello Stream"
